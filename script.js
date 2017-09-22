f2ddf1c53975e9b124eb9b1e2792b20a746f1025
Axel <axosar@gmail.com>
2017-09-22 17:57:45 -0500

    Angular test
    Angular test

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hola");
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "main.htm"
+    })
+    .when("/red", {
+        templateUrl : "red.htm"
+    })
+    .when("/green", {
+        templateUrl : "green.htm"
+    })
+    .when("/blue", {
+        templateUrl : "blue.htm"
+    });
+});
